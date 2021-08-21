e2568bef9d284127ec5d37270590bbaed001e263
Piotr Gawron <60919100+norwagov@users.noreply.github.com>
2021-08-21 14:59:09 +0200

    build prerelase alpha 0.1.2
    Uploading build

---
+++ Assets/scene_change.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_change : MonoBehaviour {
+
+	public void change(int sceneid){
+
+		SceneManager.LoadScene(sceneid);
+
+	}
+
+}
